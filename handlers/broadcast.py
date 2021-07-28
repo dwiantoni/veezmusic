3bc9c2afff554074c712e52278e2f158d4dd2575
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-28 11:45:59 +0700

    fix gcast

--- handlers/broadcast.py
+++ handlers/broadcast.py
@@ -1,7 +1,9 @@
 import asyncio
+
 from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
+
 from config import SUDO_USERS
 from helpers.filters import command
 from callsmusic.callsmusic import client as USER
@@ -14,17 +16,17 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Starting a broadcast...`")
+        wtf = await message.reply("`starting a broadcast...`")
         if not message.reply_to_message:
-            await wtf.edit("Please Reply to a Message to broadcast!")
+            await wtf.edit("please reply to a message to broadcast!")
             return
         lmao = message.reply_to_message.text
         async for dialog in USER.iter_dialogs():
             try:
                 await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`Broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+                await wtf.edit(f"`broadcasting...` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await message.reply_text(f"`Broadcast Finished` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+                await message.reply_text(f"`broadcast finished` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
