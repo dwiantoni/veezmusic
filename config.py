d78ec258d754a0a1fba569315f9314a40579c400
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-29 16:19:50 +0700

    update config

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
+import os
 from os import getenv
 from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "Veez Music")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/d91623e9cefcef8fb3aa8.png")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
