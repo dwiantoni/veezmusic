e38a7c75182caf80dadc0f6a6c234ac4c7f1a251
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-29 16:17:21 +0700

    costum background

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from pyrogram import Client as Bot
 
 from callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 
-
+response = requests.get(BG_IMAGE)
+with open("./etc/foreground.png", "wb") as file:
+    file.write(response.content)
 bot = Bot(
     ":memory:",
     API_ID,
