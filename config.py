5c846b02c1abde6ad83441f7757130377395fafc
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-29 15:06:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Veez Music Bot")
+BOT_NAME = getenv("BOT_NAME", "Veez Music")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
