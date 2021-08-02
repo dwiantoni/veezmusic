5cb2c4505862112176af003472847d607e066c19
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-02 10:47:55 +0700

    .

--- handlers/play.py
+++ handlers/play.py
@@ -30,6 +30,7 @@ ARQ_API_KEY = "YPHODY-ZLSHSE-UBBIQA-YFLDKM-ARQ"
 aiohttpsession = aiohttp.ClientSession()
 arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
 DISABLED_GROUPS = []
+useer ="NaN"
 
 def cb_admin_check(func: Callable) -> Callable:
     async def decorator(client, cb):
