8024c74c73708f4e3d8f5db945acf602d80c8823
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-02 07:44:09 +0700

    .

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -2,15 +2,16 @@ from os import path
 
 from youtube_dl import YoutubeDL
 
-from config import BOT_NAME as bn, DURATION_LIMIT
+from config import DURATION_LIMIT
 from helpers.errors import DurationLimitError
+
+
 ydl_opts = {
-    "format": "bestaudio[ext=m4a]",
+    "format": "bestaudio/best",
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
 }
-
 ydl = YoutubeDL(ydl_opts)
 
 
@@ -20,12 +21,8 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"❌ videos longer than {DURATION_LIMIT} minute(s) aren't allowed to play, the provided video is {duration} minute(s)"
         )
-    try:
-        ydl.download([url])
-    except:
-        raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
+
+    ydl.download([url])
     return path.join("downloads", f"{info['id']}.{info['ext']}")
