1181c90cb4d9cddb25e87650713e2acc37b8de9f
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-02 21:13:01 +0700

    pushed new updates

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -2,16 +2,16 @@ from os import path
 
 from youtube_dl import YoutubeDL
 
-from config import DURATION_LIMIT
-from helpers.errors import DurationLimitError
-
+from GeezProject.config import DURATION_LIMIT
+from GeezProject.helpers.errors import DurationLimitError
 
 ydl_opts = {
-    "format": "bestaudio/best",
+    "format": "bestaudio[ext=m4a]",
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
 }
+
 ydl = YoutubeDL(ydl_opts)
 
 
@@ -21,8 +21,12 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ videos longer than {DURATION_LIMIT} minute(s) aren't allowed to play, the provided video is {duration} minute(s)"
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
-
-    ydl.download([url])
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+        )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
