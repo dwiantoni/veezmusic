6e8d5a3e40eed6abd1f524e7e9481906cfb04015
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-03 09:07:03 +0700

    fixes (1)

--- handlers/play.py
+++ handlers/play.py
@@ -753,7 +753,7 @@ async def lol_cb(b, cb):
         appendable = [s_name, r_by, loc]
         qeue.append(appendable)
         await cb.message.delete()
-        await cb.message.send_photo(chat_id,
+        await cb.send_photo(chat_id,
             photo="final.png",
             caption = f"🏷 **Judul:** [{title[:60]}]({url})\n⏱ **Durasi:** {duration}\n💡 **Status:** Antrian Ke `{position}`\n" \
                     + f"🔮 **Permintaan:** {r_by.mention}",
@@ -773,7 +773,7 @@ async def lol_cb(b, cb):
 
         callsmusic.pytgcalls.join_group_call(chat_id, file_path)
         await cb.message.delete()
-        await cb.message.send_photo(chat_id,
+        await cb.send_photo(chat_id,
             photo="final.png",
             caption = f"🏷 **Judul:** [{title[:60]}]({url})\n⏱ **Durasi:** {duration}\n💡 **Status:** Sedang Memutar\n" \
                     + f"🔮 **Permintaan:** {r_by.mention}",
